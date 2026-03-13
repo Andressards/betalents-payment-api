13ee30686474f864f9aa1dca407ba8a85eab3cf6
Andressards <andressarodrigues.profissional@gmail.com>
2026-03-13 15:33:22 -0300

    feat: criação dos models

--- src/app/Models/Transaction.php
+++ src/app/Models/Transaction.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'client_id',
+        'gateway',
+        'external_id',
+        'status',
+        'amount',
+        'card_last_numbers'
+    ];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'transaction_products')
+            ->withPivot('quantity');
+    }
 }
--- src/app/Models/TransactionProduct.php
+++ src/app/Models/TransactionProduct.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TransactionProduct extends Model
 {
-    use HasFactory;
+    protected $table = 'transaction_products';
+
+    protected $fillable = [
+        'transaction_id',
+        'product_id',
+        'quantity'
+    ];
 }
